f15b6c244b2e7c727f6bd952f5623b30bdbfbb95
ryannarnoldd <ryannarnoldd@gmail.com>
2023-01-10 09:05:47 -0500

    Temporary changing of path names

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import {
 import Navbar from './components/Navbar';
 import Daily from './pages/Daily';
 import Random from './pages/Random';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 import About from './pages/About';
 
 function App() {
 
 
   return (
-    <Router>
+    <Router basename={'FaithTasks'}>
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path="/" element={<Random />} />
-          <Route path="daily" element={<Daily />} />
-          <Route path="random" element={<Random />} />
-          <Route path="about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/daily" element={<Daily />} />
+          <Route path="/random" element={<Random />} />
+          <Route path="/about" element={<About />} />
           {/* <Route path="/:year" element={<h1 style={{ color: 'red' }}>Year</h1>} />
           <Route path="/:year/:month" element={<h1 style={{ color: 'red' }}>Month</h1>} />
           <Route path="/:year/:month/:day" element={<h1 style={{ color: 'red' }}>Month</h1>} /> */}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = () => {
 
   return (
     <nav>
-      <a href="/">Faith Tasks</a>
+      <a href="/FaithTasks/">Faith Tasks</a>
       <div>
-        <a href="/daily">Daily</a>
-        <a href="/random">Random</a>
-        <a href="/about">About</a>
+        <a href="/FaithTasks/daily">Daily</a>
+        <a href="/FaithTasks/random">Random</a>
+        <a href="/FaithTasks/about">About</a>
       </div>
     </nav>
   );
