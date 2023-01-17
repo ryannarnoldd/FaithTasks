a6db06a85e5db8f4844a982836585136005c7378
ryannarnoldd <ryannarnoldd@gmail.com>
2023-01-17 12:02:18 -0500

    Small changes to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,9 +16,9 @@ const About = () => {
 
       <h3>We also have a <a href="https://discord.gg/your-server-link" class="btn">Discord community</a> to share thoughts and struggles with our daily tasks. <br />We would love to have you join us!</h3>
 
-      <h3>I attend <a href="https://www.calvarychapelhighland.com/">Calvary Chapel Highland</a>. Click the website if interested in joining!</h3>
+      <h3>I attend <a href="https://www.calvarychapelhighland.com/">Calvary Chapel Highland</a>. Click if interested in joining!</h3>
 
-      <h3>If you have any questions or feedback, please don't hesitate to reach out to <a href="mailto:FaithTasks@gmail.com">FaithTasks@gmail.com</a></h3>
+      <h3>If you have any questions or feedback, please don't hesitate to reach out at <a href="mailto:FaithTasks@gmail.com">FaithTasks@gmail.com</a></h3>
 
       <h3>Follow us on <a href="https://www.instagram.com/faithtasks">Instagram</a> for daily updates!</h3>
 
